94c687c09d133f0b039ce71361b5cc7d294774b2
Ubuntu <ubuntu@ip-172-31-28-136.us-east-2.compute.internal>
2020-08-08 03:24:10 +0000

    update variable

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
   version = "=2.4.0"
 
   subscription_id = var.ARM_SUBSCRIPTION_ID
-  client_id       = var.ARM_CLIENT_SECRET
+  client_id       = var.ARM_CLIENT_ID
   client_secret   = var.ARM_CLIENT_SECRET
   tenant_id       = var.ARM_TENANT_ID
 
--- variables.tf
+++ variables.tf
@@ -6,8 +6,8 @@ variable "ARM_CLIENT_SECRET" {
     description = "ARM_CLIENT_SECRET"
 }
 
-variable "ARM_CLIENT_SECRET" {
-    description = "ARM_CLIENT_SECRET"
+variable "ARM_CLIENT_ID" {
+    description = "ARM_CLIENT_ID"
 }
 variable "ARM_TENANT_ID" {
     description = "ARM_TENANT_ID"
