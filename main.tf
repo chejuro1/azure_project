4a005632957359ad0302c4eb0f1ce3c7b283c9c9
Ubuntu <ubuntu@ip-172-31-28-136.us-east-2.compute.internal>
2020-08-08 03:35:23 +0000

    update variable

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the ve$
-  version = "=2.4.0"
+  version = "=2.5.0"
 
   subscription_id = var.ARM_SUBSCRIPTION_ID
   client_id       = var.ARM_CLIENT_ID
