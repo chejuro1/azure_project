fadb5ef97b7cb2fef2f5bc7bbe9edc40fbd6baea
Ubuntu <ubuntu@ip-172-31-28-136.us-east-2.compute.internal>
2020-08-08 03:00:35 +0000

    add sp

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the ve$
+  version = "=2.5.0"
+  features {}
+}
+
+
+variable "client_secret" {
+}
+
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the ve$
+  version = "=2.4.0"
+
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_SECRET
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+
