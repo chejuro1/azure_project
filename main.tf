970f8a5a320530a6fe3322491bac47089429a926
Ubuntu <ubuntu@ip-172-31-28-136.us-east-2.compute.internal>
2020-08-08 04:49:34 +0000

    update

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the ve$
   version = "=2.0"
 
-  subscription_id = "{var.subscription_id}"
-  client_id       = "{var.client_id}"
-  client_secret   = "{var.client_secret}"
-  tenant_id       = "{var.tenant_id}" 
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id 
   features {}
 }
 
